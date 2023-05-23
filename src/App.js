bd0f17a895f91083d438415c09c501da11d6b490
zanetowell <zanetowell@gmail.com>
2023-05-23 17:29:07 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import About from './components/About'
+import Contact from './components/Contact'
+import Experience from './components/Experience'
+import Nav from './components/Nav'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
