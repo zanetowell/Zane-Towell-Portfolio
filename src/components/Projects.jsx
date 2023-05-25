4816f2b6c5954634b2fb60ef4880fb2544d27010
zanetowell <zanetowell@gmail.com>
2023-05-25 19:01:59 -0400

    adds links to deployed projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,29 +13,37 @@ const Projects = () => {
         <div className='project-1'>
           <img src={poke} height='300px' alt=''/>
           <div className='p1info'>
+            <a href='https://zanetowell.github.io/Poke-Library/' target='_blank' rel='noreferrer'>
             <h3>Poké Library</h3>
             <article>A fun simple app to let users search any pokemon, see their base stats, and add the pokemon to their party!</article>
+            </a>
           </div>
         </div>
         <div className='project-2'>
           <img src={waters} height='300px' alt=''/>
           <div className='p2info'>
+            <a href='https://waters-reviewed.herokuapp.com/' target='_blank' rel='noreferrer'>
             <h3>Waters:Reviewed</h3>
             <article>My first project to use Node, Express, and MongoDB. A fun way to learn RESTful routing, it allows users to find or add their favorite waters and leave reviews!</article>
+            </a>
           </div>
         </div>
         <div className='project-3'>
           <img src={sandwich} height='300px' alt=''/>
           <div className='p3info'>
+            <a href='https://sandwich-io.herokuapp.com/' target='_blank' rel='noreferrer'>
             <h3>Sandwich.IO</h3>
             <article>A group project built with Python, Django, and PostgreSQL. This app allows users to add ingredients and craft the sandwich of their wildest dreams!</article>
+            </a>
           </div>
         </div>
         <div className='project-4'>
           <img src={curate} height='300px' alt=''/>
           <div className='p4info'>
+            <a href='https://curate-dev.netlify.app/' target='_blank' rel='noreferrer'>
             <h3>Curate</h3>
             <article>A MERN stack app that uses Firebase to implement full authentication and authorization. It allows users to create personalized study topics and materials! There's even Lofi!</article>
+            </a>
           </div>
         </div>
       </div>
