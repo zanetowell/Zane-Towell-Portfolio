ffa36821c877c68f8f401c620130308c743ca2e4
zanetowell <zanetowell@gmail.com>
2023-06-01 14:18:03 -0400

    first draft of about set for mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,13 +11,11 @@ const About = () => {
       <article className='about-blurb'>
         <h2 className='about-me'>About Me</h2>
         <div className='divider'></div>
-        <h3>Hi, my name is Zane!
+        <h3><b>Hi, my name is Zane!</b>
           <br />
             I'm a developer that's passionate about creating effective frontend designs and efficient backends. Collaboration is at the heart of what I do.
-            <br />
             I thrive in dynamic, diverse teams where different perspectives come together to create something extraordinary.
-            <br />
-             I love working with clients and colleagues, and I believe in fostering open communication and strong relationships to achieve shared goals.
+            I love working with clients and colleagues, and I believe in fostering open communication and strong relationships to achieve our shared goals.
         </h3>
       </article>
     </div>
